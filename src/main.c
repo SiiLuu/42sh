b278c81b88a1c41a8d536ceebab013d507ca228b
Hugo Caulfield <hugo.caulfield@epitech.eu>
2019-05-07 19:08:34 +0200

    [MODIF] main.c / Add error command

--- src/main.c
+++ src/main.c
@@ -182,27 +182,29 @@ int main(int argc, char const **argv, char **env)
     int i = 0;
 
     env = empty_env(env);
-    while (1)
-        while (1) {
-            signal(SIGINT, control_c);
-            if ((str = get_input()) == NULL)
-                return (0);
-            tab = array_memory(tab, str);
-            tab = my_str_to_word_array(str, tab);
-            if (env_modif(env, tab) || change_directory(tab))
+    while (1) {
+        signal(SIGINT, control_c);
+        if ((str = get_input()) == NULL)
+            return (0);
+        tab = array_memory(tab, str);
+        tab = my_str_to_word_array(str, tab);
+        if (env_modif(env, tab) || change_directory(tab))
+            break;
+        i = 0;
+        pathtab2 = get_path(env);
+        while (pathtab2[i] != NULL) {
+            pathtab2[i] = my_strcat(pathtab2[i], "/");
+            pathtab2[i] = my_strcat(pathtab2[i], tab[0]);
+            if (access(pathtab2[i], F_OK) == 0) {
+                pathtab = pathtab2[i];
                 break;
-            pathtab2 = get_path(env);
-            while (pathtab2[i] != NULL) {
-                pathtab2[i] = my_strcat(pathtab2[i], "/");
-                pathtab2[i] = my_strcat(pathtab2[i], tab[0]);
-                if (access(pathtab2[i], F_OK) == 0) {
-                    pathtab = pathtab2[i];
-                    break;
-                }
-                i++;
             }
-            main_execution(pathtab, tab, env, str);
-            free(tab);
+            i++;
         }
+        if (access(pathtab2[i], F_OK) == -1)
+            pathtab = str;
+        main_execution(pathtab, tab, env, str);
+        free(tab);
+    }
     return (0);
 }
