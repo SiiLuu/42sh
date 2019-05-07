5741ad72b5729018572595841bae5b5bb1109b1e
Lucas Simao <lsimao@Lucas.simao>
2019-05-07 18:37:50 +0200

    [MODIF] src/main.c / readd the isatty func.

--- src/main.c
+++ src/main.c
@@ -7,6 +7,25 @@
 
 #include "../include/my.h"
 
+char *inputs(char *line, ssize_t read)
+{
+    if (isatty(0)) {
+        if (!my_strcmp(line, "exit") || read == -1) {
+            my_printf("exit\n");
+            return (NULL);
+        }
+        if (read == -1) {
+            my_printf("exit");
+            return (NULL);
+        }
+    }
+    else {
+        if (!my_strcmp(line, "exit") || read == -1)
+            return (NULL);
+        if (read == -1)
+            return (NULL);
+    }
+}
 
 char *get_input(void)
 {
@@ -14,20 +33,14 @@ char *get_input(void)
     size_t len = 0;
     ssize_t read = 0;
 
-    my_putstr("$ ~ ");
+    if (isatty(0))
+        my_putstr("$ ~ ");
     read = getline(&line, &len, stdin);
     if (line[read - 1] == '\n') {
         line[read] = 0;
         line[read - 1] = '\0';
     }
-    if (!my_strcmp(line, "exit") || read == -1) {
-        my_printf("exit\n");
-        return (NULL);
-    }
-    if (read == -1) {
-        my_printf("exit");
-        return (NULL);
-    }
+    line = inputs(line, read);
     return (line);
 }
 
