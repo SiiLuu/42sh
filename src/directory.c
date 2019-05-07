9cedea3891caef22935209b72b34ec0b5637fe49
Hugo Caulfield <hugo.caulfield@epitech.eu>
2019-05-07 18:30:36 +0200

    [MODIF] main.c / Modif the gesture of path

--- src/directory.c
+++ src/directory.c
@@ -48,46 +48,10 @@ char *find_path(char **env, char **tab)
     return (NULL);
 }
 
-/*char *init_path(char *path, char *env)
-{
-    int	i = 0;
-    int	j = 0;
-
-    while (env[i] != '=' && env[i] != '\0')
-        i++;
-    i++;
-    while (env[i] != '\0') {
-        path[j] = env[i];
-        i++;
-        j++;
-    }
-    path[j] = '\0';
-    return (path);
-}
-
-char **get_path(char **env)
-{
-    int	i = 0;
-    char **res = NULL;
-    char *path = NULL;
-
-    while (env[i] != NULL) {
-        if (env[i][0] == 'P' && env[i][1] == 'A'
-            && env[i][2] == 'T' && env[i][3] == 'H') {
-            if ((path = malloc(sizeof(char *) * my_strlen(env[i]))) == NULL)
-                exit(84);
-            path = init_path(path , env[i]);
-            res = str_to_word_tab_limited(path, ':');
-        }
-        i++;
-    }
-    return (res);
-}*/
-
 char **empty_env(char **env)
 {
     if (env[0] == 0) {
-        env[0] = "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin";
+        env[0] = "PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin";
         env[1] = NULL;
     }
     return (env);
@@ -104,7 +68,7 @@ int change_directory(char **tab)
             tab[1] = "/home/hugo\0";
         my_strcat(buff, tab[1]);
         if (chdir(buff) == -1)
-            my_printf("%s: Not a directory.\n", tab[1]);
+            my_printf("%s: No such file or directory.\n", tab[1]);
         return (1);
     }
     return (0);
@@ -117,11 +81,11 @@ int main_execution(char *pathtab, char **tab, char **env, char *str)
 
     if (check_exist(pathtab, str))
         return (1);
+    if (my_strlen(str) == 0)
+        return (0);
     pid = fork();
     if (pid > 0)
         wait(0);
-    if (my_strlen(str) == 0)
-        return (0);
     else if (pid == 0)
         if (execve(pathtab, tab, env) == -1) {
             stat(pathtab, &sd);
--- src/main.c
+++ src/main.c
@@ -7,27 +7,6 @@
 
 #include "../include/my.h"
 
-char *inputs(char *line, ssize_t read)
-{
-    if (isatty(0)) {
-        if (!my_strcmp(line, "exit") || read == -1) {
-            my_printf("exit\n");
-            return (NULL);
-        }
-        if (read == -1) {
-            my_printf("exit");
-            return (NULL);
-        }
-    }
-    else {
-        if (!my_strcmp(line, "exit") || read == -1) {
-            return (NULL);
-        }
-        if (read == -1)
-            return (NULL);
-    }
-    return (line);
-}
 
 char *get_input(void)
 {
@@ -35,14 +14,20 @@ char *get_input(void)
     size_t len = 0;
     ssize_t read = 0;
 
-    if (isatty(0))
-        my_putstr("$ ~ ");
+    my_putstr("$ ~ ");
     read = getline(&line, &len, stdin);
     if (line[read - 1] == '\n') {
         line[read] = 0;
         line[read - 1] = '\0';
     }
-    line = inputs(line, read);
+    if (!my_strcmp(line, "exit") || read == -1) {
+        my_printf("exit\n");
+        return (NULL);
+    }
+    if (read == -1) {
+        my_printf("exit");
+        return (NULL);
+    }
     return (line);
 }
 
@@ -83,11 +68,104 @@ char **array_memory2(char **array, char const *str)
     return (array);
 }
 
+char **init_tab(char *str)
+{
+    char **tab = NULL;
+
+    if ((tab = malloc(sizeof(char *) * (my_strlen(str) + 1))) == NULL)
+        exit(84);
+    if ((tab[0] = malloc(sizeof(char) * (my_strlen(str) + 1))) == NULL)
+        exit(84);
+    return (tab);
+}
+
+char **end_tab(char **tab, int i, int j)
+{
+    tab[j][i] = '\0';
+    j++;
+    if ((tab[j] = malloc(sizeof(char) * 2)) == NULL)
+        exit(84);
+    tab[j] = NULL;
+    return (tab);
+}
+
+char **get_tab(char **tab, int i, int j, char *str)
+{
+    tab[j][i] = '\0';
+    j++;
+    if ((tab[j] = malloc(sizeof(char) * (my_strlen(str) + 1))) == NULL)
+        exit(84);
+    return (tab);
+}
+
+char **str_to_word_tab_limited(char *str, char limit)
+{
+    int	i = 0;
+    char **tab = NULL;
+    int	j = 0;
+    int	k = 0;
+
+    tab = init_tab(str);
+    while (str[i] != '\0') {
+        if ((str[i] == limit) && (str[i + 1] != '\0')) {
+            tab = get_tab(tab, j, k, str);
+            k++;
+            j = 0;
+            i++;
+        }
+        if (str[i] != '\0') {
+            tab[k][j] = str[i];
+            j++;
+            i++;
+        }
+    }
+    tab = end_tab(tab, j, k);
+    return (tab);
+}
+
+char *init_path(char *path, char *env)
+{
+    int	i = 0;
+    int	j = 0;
+
+    while (env[i] != '=' && env[i] != '\0')
+        i++;
+    i++;
+    while (env[i] != '\0') {
+        path[j] = env[i];
+        i++;
+        j++;
+    }
+    path[j] = '\0';
+    return (path);
+}
+
+char **get_path(char **env)
+{
+    int	i = 0;
+    char **res = NULL;
+    char *path = NULL;
+
+    while (env[i] != NULL) {
+        if (env[i][0] == 'P' && env[i][1] == 'A'
+            && env[i][2] == 'T' && env[i][3] == 'H') {
+            if ((path = malloc(sizeof(char *) * my_strlen(env[i]))) == NULL)
+                exit(84);
+            path = init_path(path , env[i]);
+            res = str_to_word_tab_limited(path, ':');
+        }
+        i++;
+    }
+    return (res);
+}
+
 int main(int argc, char const **argv, char **env)
 {
     char *str = 0;
     char **tab = 0;
     char *pathtab = 0;
+    char **pathtab2 = 0;
+    int i = 0;
 
     env = empty_env(env);
     while (1)
@@ -97,11 +175,18 @@ int main(int argc, char const **argv, char **env)
                 return (0);
             tab = array_memory(tab, str);
             tab = my_str_to_word_array(str, tab);
-            pathtab = find_path(env, tab);
             if (env_modif(env, tab) || change_directory(tab))
                 break;
-            if (access(pathtab, F_OK) == -1)
-                pathtab = str;
+            pathtab2 = get_path(env);
+            while (pathtab2[i] != NULL) {
+                pathtab2[i] = my_strcat(pathtab2[i], "/");
+                pathtab2[i] = my_strcat(pathtab2[i], tab[0]);
+                if (access(pathtab2[i], F_OK) == 0) {
+                    pathtab = pathtab2[i];
+                    break;
+                }
+                i++;
+            }
             main_execution(pathtab, tab, env, str);
             free(tab);
         }
