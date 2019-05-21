14d35329764b29e0a1c78385a75cf32191ddfd8d
Julien Salies <J.Salies@localhost.localdomain>
2019-05-21 14:29:22 -0400

    [ADDED] in directory.c / echo gestion with the return value

--- src/directory.c
+++ src/directory.c
@@ -39,21 +39,27 @@ int change_directory(char **tab)
     return (status);
 }
 
-void catch_seg_fault(pid_t pid)
+int catch_seg_fault(pid_t pid, int return_value)
 {
     pid_t w = 0;
     int status = 0;
 
     if ((w = waitpid(pid, &status, 0)) != -1) {
-        if (WTERMSIG(status) == 8 && WCOREDUMP(status))
-            my_printf("Floating point exception (core dumped)\n");
-        if (WTERMSIG(status) == 11 && WCOREDUMP(status))
-            my_printf("Segmentation fault (core dumped)\n");
-        if (WTERMSIG(status) == 8 && !WCOREDUMP(status))
-            my_printf("Floating point exception\n");
-        if (WTERMSIG(status) == 11 && !WCOREDUMP(status))
-            my_printf("Segmentation fault\n");
+        if (WIFEXITED(status))
+            return_value = WEXITSTATUS(status);
+        else {
+            return_value = status;
+            if (WTERMSIG(status) == 8 && WCOREDUMP(status))
+                my_printf("Floating point exception (core dumped)\n");
+            if (WTERMSIG(status) == 11 && WCOREDUMP(status))
+                my_printf("Segmentation fault (core dumped)\n");
+            if (WTERMSIG(status) == 8 && !WCOREDUMP(status))
+                my_printf("Floating point exception\n");
+            if (WTERMSIG(status) == 11 && !WCOREDUMP(status))
+                my_printf("Segmentation fault\n");
+        }
     }
+    return (return_value);
 }
 
 void check_file_format(char *pathtab, char *str, pid_t pid)
@@ -67,18 +73,30 @@ void check_file_format(char *pathtab, char *str, pid_t pid)
     kill(pid, SIGKILL);
 }
 
+int check_wrong_command(char *pathtab, char *str)
+{
+    if (check_exist(pathtab, str))
+        return (1);
+    if (my_strlen(str) == 0)
+        return (1);
+    return (0);
+}
+
 int main_execution(char *pathtab, char **tab, char **env, char *str)
 {
     pid_t pid = 0;
     int i = 0;
+    static int return_value = 0;
 
-    if (check_exist(pathtab, str))
+    if (strcmp(tab[0], "echo") == 0 && strcmp(tab[1], "$?") == 0) {
+        printf("%d\n", return_value);
         return (1);
-    if (my_strlen(str) == 0)
+    }
+    if (check_wrong_command(pathtab, str))
         return (0);
     pid = fork();
     if (pid > 0)
-        catch_seg_fault(pid);
+        return_value = catch_seg_fault(pid, return_value);
     else if (pid == 0) {
         i = execve(pathtab, tab, env);
         if (errno == 8)
