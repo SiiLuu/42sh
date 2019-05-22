ab59dbc6055f6a4f3f8d9c08fe19ed84df4a66a3
Leyton Ly <leyton.ly@epitech.eu>
2019-05-22 18:23:21 +0200

    [MODIFIED] manage_cd.c / Can go to the home and folders in it or in the root folder

--- src/manage_cd.c
+++ src/manage_cd.c
@@ -7,9 +7,14 @@
 
 #include "my.h"
 
-int manage_cd_params(char **tab, char *buff, size_t b)
+int manage_cd_params(char **tab, char *buff, size_t b, char *home_str)
 {
-    if (tab[0][0] == 'c' && tab[0][1] == 'd' && tab[0][2] == '\0' &&
+    if ((tab[0][0] == 'c' && tab[0][1] == 'd' && tab[0][2] == '\0') &&
+        (tab[1] == NULL || tab[1][0] == '~')) {
+        cd_home(tab, buff, b, home_str);
+        return (1);
+    }
+    else if (tab[0][0] == 'c' && tab[0][1] == 'd' && tab[0][2] == '\0' &&
         tab[1] != NULL) {
         if (tab[1][0] != '/') {
             getcwd(buff, b);
@@ -23,9 +28,52 @@ int manage_cd_params(char **tab, char *buff, size_t b)
             dprintf(2, "%s: Not a directory.\n", tab[1]);
         return (1);
     }
-    else if (tab[0][0] == 'c' && tab[0][1] == 'd' && tab[0][2] == '\0' &&
-            tab[1] == NULL) {
-                chdir("/");
-            }
     return (0);
+}
+
+int cd_home(char **tab, char *buff, size_t b, char *home_str)
+{
+    int i = 1;
+    int j = my_strlen(home_str);
+    char *h_path = NULL;
+
+    if (tab[1] == NULL)
+        chdir(home_str);
+    else {
+        h_path = malloc(sizeof(char) * (my_strlen(tab[1]) + \
+        my_strlen(home_str)));
+        my_strcpy(h_path, home_str);
+        for (i = 1;  tab[1][i] != '\0'; i++, j++) {
+            h_path[j] = tab[1][i];
+        }
+        chdir(h_path);
+        free(h_path);
+    }
+    return (1);
+
+}
+
+char *get_home_env(char **env)
+{
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    char *home = NULL;
+
+    for (i = 0; env[i] != NULL; i++) {
+        if (env[i][0] == 'H' && env[i][1] == 'O' &&
+        env[i][2] == 'M' && env[i][3] == 'E' && env[i][4] == '=')
+            break;
+    }
+    if (env[i][0] == 'H' && env[i][1] == 'O' &&
+        env[i][2] == 'M' && env[i][3] == 'E' &&
+        env[i][4] == '=') {
+        home = malloc(sizeof(char) * my_strlen(env[i]) - 4);
+        for (j = 0; env[i][j] != '='; j++);
+        j = j + 1;
+        for (k = 0; env[i][j] != '\0'; k++, j++) {
+            home[k] = env[i][j];
+        }
+    }
+    return (home);
 }
