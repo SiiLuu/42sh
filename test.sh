f9c49ba0d83754fc2f53cd01cd7134c43f138832
Julien Salies <J.Salies@localhost.localdomain>
2019-05-28 13:15:42 -0400

    [MODIFIED] test.sh / push

---
+++ test.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+##comment
+
+echo "ls" | ./42sh c
+if [ $? == 0 ]
+then
+    echo "TEST : PASSED"
+else
+    echo "TEST : FAILED"
+fi
+echo
+
+echo "ls ; ls -a" | ./42sh c
+if [ $? == 0 ]
+then
+    echo "TEST : PASSED"
+else
+    echo "TEST : FAILED"
+fi
+echo
+
+echo "ls ; ls | grep src | wc -l" | ./42sh c
+if [ $? == 0 ]
+then
+    echo "TEST : PASSED"
+else
+    echo "TEST : FAILED"
+fi
+echo
+
+echo "ls | grep src | wc -l ; ls" | ./42sh c
+if [ $? == 0 ]
+then
+    echo "TEST : PASSED"
+else
+    echo "TEST : FAILED"
+fi
+echo
+
+echo "ls ; ls -a ; ls | grep src | wc -l ; ls ; cd include ; ls" | ./42sh c
+if [ $? == 0 ]
+then
+    echo "TEST : PASSED"
+else
+    echo "TEST : FAILED"
+fi
+echo
+
+echo "ls | grep src | wc -l ; ls -a" | ./42sh c
+if [ $? == 0 ]
+then
+    echo "TEST : PASSED"
+else
+    echo "TEST : FAILED"
+fi
+echo
+
+echo "cd include ; ls ; cd .. ; ls ; pwd ; ls | grep src | wc -l ; ls -a" | ./42sh c
+if [ $? == 0 ]
+then
+    echo "TEST : PASSED"
+else
+    echo "TEST : FAILED"
+fi
+echo
+
+echo "cd include ; ls ; cd .. ; ls -a ; pwd ; ls | grep src | wc -l ; id ; who" | ./42sh c
+if [ $? == 0 ]
+then
+    echo "TEST : PASSED"
+else
+    echo "TEST : FAILED"
+fi
+echo
+
+echo "ls ; ls -a ; ls | grep src | wc -l" | ./42sh c
+if [ $? == 0 ]
+then
+    echo "TEST : PASSED"
+else
+    echo "TEST : FAILED"
+fi
+echo
