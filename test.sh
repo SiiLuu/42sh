da6f1a8e73f10ed1b3351e19adab74542145bc32
Lucas Simao <lsimao@Lucas.simao>
2019-05-31 11:59:52 +0200

    ah

--- test.sh
+++ test.sh
@@ -63,3 +63,12 @@ else
     echo "TEST : FAILED"
 fi
 echo
+
+echo "ls ; ls -a ; ls | grep src | wc -l ; cd src ; ls -a ; cd .. ; ls -a" | ./42sh c
+if [ $? == 0 ]
+then
+    echo "TEST : PASSED"
+else
+    echo "TEST : FAILED"
+fi
+echo
