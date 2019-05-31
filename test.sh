0fc3b19a8b8e35636687d5b88a3f4fcd9d6b2a3a
Lucas Simao <lsimao@Lucas.simao>
2019-05-31 11:02:06 +0200

    verif

--- test.sh
+++ test.sh
@@ -55,24 +55,6 @@ else
 fi
 echo
 
-echo "cd include ; ls ; cd .. ; ls ; pwd ; ls | grep src | wc -l ; ls -a" | ./42sh c
-if [ $? == 0 ]
-then
-    echo "TEST : PASSED"
-else
-    echo "TEST : FAILED"
-fi
-echo
-
-echo "cd include ; ls ; cd .. ; ls -a ; pwd ; ls | grep src | wc -l ; id ; who" | ./42sh c
-if [ $? == 0 ]
-then
-    echo "TEST : PASSED"
-else
-    echo "TEST : FAILED"
-fi
-echo
-
 echo "ls ; ls -a ; ls | grep src | wc -l" | ./42sh c
 if [ $? == 0 ]
 then
